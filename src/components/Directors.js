ce728015461d14bb00620da8f9d1b9aa4103f166
Henry Escobar <henrye@gmail.com>
2022-08-22 15:37:21 -0700

    commit tree dd2a65b7b13bb97d7b16497dd88a66af5e5332d1

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorList = directors.map((director, index) => {
+    return (
+      <div key={index}>
+        <h2>{director.name}</h2>
+        <ul>
+          {director.movies.map((movie) => {
+            return <li key={movie}>{movie}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorList}
+    </div>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieList = movies.map((movie, index) => {
+    return (
+      <div key={index}>
+        <h2>{movie.title}</h2>
+        <p>Runtime: {movie.time}</p>
+        <ul>
+          {movie.genres.map((genre) => {
+            return <li key={genre}>{genre}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieList}
+    </div>
+  );
 }
 
 export default Movies;
